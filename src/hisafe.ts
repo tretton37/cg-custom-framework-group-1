602dfa5f7158e29137ebf5f6bc214030b73da62c
Erik Moberg <extern.tretton37-moberg@universal-investment.com>
2022-10-14 16:12:27 +0200

    wip

--- src/hisafe.ts
+++ src/hisafe.ts
@@ -1,3 +1,7 @@
-export default function hisafe(tagname: string, props: any, children: any) {
-  //
+export default function hisafe(tagname: string, props: any, ...children: any) {
+
+  const element = document.createElement(tagname);
+  element.innerText = children;
+  document.getElementById("app")!.appendChild(element);
+  
 }
