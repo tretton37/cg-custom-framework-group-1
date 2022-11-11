602f0bfd4bd91e90ce1856800ea1951727b5157d
IvaTsu <ivan.tsud@1337.tech>
2022-11-11 16:47:53 +0100

    feat: add styles

--- src/hisafe-element.tsx
+++ src/hisafe-element.tsx
@@ -3,22 +3,26 @@ export abstract class HisafeElement<TState extends object> extends HTMLElement {
     super();
     this.attachShadow({ mode: "open" });
     this.state = defaultState;
-    
   }
 
   _state!: TState;
   set state(val: TState) {
     const render = this.render;
     this._state = new Proxy(val, {
-        set(target: TState, p: string | symbol, value: any, receiver: any): boolean {
-          target[p] = value
-          render();
-          return true
-        },
-        get(target: TState, p: string | symbol, receiver: any): any {
-          return target[p]
-        }
-      })
+      set(
+        target: TState,
+        p: string | symbol,
+        value: any,
+        receiver: any
+      ): boolean {
+        target[p] = value;
+        render();
+        return true;
+      },
+      get(target: TState, p: string | symbol, receiver: any): any {
+        return target[p];
+      },
+    });
   }
   get state() {
     return this._state;
@@ -29,15 +33,22 @@ export abstract class HisafeElement<TState extends object> extends HTMLElement {
   }
 
   dispatchHisafeEvent<T>(name: string, payload: T) {
-    const event = new CustomEvent(name, { detail: { isHighSafeEvent: true, payload }, composed: true });
+    const event = new CustomEvent(name, {
+      detail: { isHighSafeEvent: true, payload },
+      composed: true,
+    });
     this.dispatchEvent(event);
   }
 
-  private render = ()=> {
+  private render = () => {
     const html = this.html();
-    this.shadowRoot!.innerHTML = "";
+    this.shadowRoot!.innerHTML = `<style>${this.css()}</style>`;
     this.shadowRoot!.appendChild(html);
-  }
+  };
 
   abstract html(): Node;
+
+  css = () => {
+    return "";
+  };
 }
--- src/todo-item-component.tsx
+++ src/todo-item-component.tsx
@@ -1,27 +1,38 @@
-import { HisafeElement } from './hisafe-element.js';
-import hisafe from './hisafe.js';
-import { TodoItem } from './todo-item.js';
+import { HisafeElement } from "./hisafe-element.js";
+import hisafe from "./hisafe.js";
+import { TodoItem } from "./todo-item.js";
 
 export class TodoItemComponent extends HisafeElement<TodoItem> {
-
   constructor() {
-    super({label:'', isDone:false, id: ''});
+    super({ label: "", isDone: false, id: "" });
   }
 
+  css = () => {
+    return `.isDone {text-decoration:line-through}`;
+  };
+
   html(): Node {
-    const myElement = <li>
-      <input type="checkbox" onChange={this.handleCheckboxChanged} checked={this.state.isDone} />
-      <span style={this.state.isDone ? 'text-decoration:line-through' : ''}>{this.state.label}</span>
-      <button onClick={this.handleDelete}>X</button>
-    </li>;
+    const myElement = (
+      <li>
+        <input
+          type="checkbox"
+          onChange={this.handleCheckboxChanged}
+          checked={this.state.isDone}
+        />
+        <span class={this.state.isDone ? "isDone" : ""}>
+          {this.state.label}
+        </span>
+        <button onClick={this.handleDelete}>X</button>
+      </li>
+    );
     return myElement;
   }
 
   handleDelete = () => {
-    this.dispatchHisafeEvent('deleteTodoItem', this.state.id)
+    this.dispatchHisafeEvent("deleteTodoItem", this.state.id);
   };
 
-  handleCheckboxChanged = () =>{
+  handleCheckboxChanged = () => {
     this.state.isDone = !this.state.isDone;
-  }
+  };
 }
