333389a1328c1318bf9b389168c2688b9cbdfef3
Andre <31482129+andre-brdoch@users.noreply.github.com>
2022-11-13 15:51:17 +0100

    Styling (#4)
    * Improve markup
    
    * Add basic styles
    
    * Add text input styling
    
    * Add item styling
    
    * Make responsive
    
    * Add more theme variables

--- src/app-component.tsx
+++ src/app-component.tsx
@@ -1,6 +1,7 @@
-import { HisafeElement } from "./hisafe-element.js";
-import hisafe from "./hisafe.js";
-import { TodoItem } from "./todo-item.js";
+import { HisafeElement } from './hisafe-element.js';
+import hisafe from './hisafe.js';
+import { TodoItem } from './todo-item.js';
+import { colors, space, font } from './theme.js';
 
 class AppComponentState {
   todoItems: TodoItem[] = [];
@@ -13,32 +14,166 @@ export class AppComponent extends HisafeElement<AppComponentState> {
 
   html(): Node {
     return (
-      <div>
-        <h1 data-name="henrik">I am a web component!!!!!</h1>
-        <p>This is a p</p>
-        <input type="text" id="name" />
-        <ul>
-          {this.state.todoItems.map((todoItem) => (
-            <todo-item-component state={todoItem} onDeleteTodoItem={this.deleteItem} />
-          ))}
-        </ul>
-        <button onClick={this.handleClick}>Click me!</button>
-      </div>
+      <main class="app">
+        <div class="card">
+          <h1>ToDo-List</h1>
+
+          <form onSubmit={this.handleSubmit} class="form">
+            <label for="todo-input" class="add-label">
+              What needs to be done?
+            </label>
+            <input type="text" id="todo-input" class="add-input" />
+            <input type="submit" value="Add" class="add-button" />
+          </form>
+
+          <ul>
+            {this.state.todoItems.map((todoItem) => (
+              <todo-item-component
+                state={todoItem}
+                onDeleteTodoItem={this.deleteItem}
+              />
+            ))}
+          </ul>
+        </div>
+      </main>
     );
   }
 
-  deleteItem = (id: string) => {
-    this.state.todoItems = this.state.todoItems.filter(todoItem => todoItem.id !== id);
-  }
+  handleSubmit = (e: Event) => {
+    e.preventDefault();
 
-  handleClick = () => {
     const input: HTMLInputElement = this.shadowRoot!.getElementById(
-      "name"
+      'todo-input'
     ) as HTMLInputElement;
-    this.state.todoItems = [...this.state.todoItems, {
-            isDone: false,
-            label: input.value,
-            id: Math.random().toString()
-        }]
+    const label = input.value;
+
+    if (!label) {
+      return;
+    }
+
+    const newTodoItem: TodoItem = {
+      isDone: false,
+      label,
+      id: Math.random().toString(),
+    };
+    this.state.todoItems = [...this.state.todoItems, newTodoItem];
+  };
+
+  deleteItem = (id: string) => {
+    this.state.todoItems = this.state.todoItems.filter(
+      (todoItem) => todoItem.id !== id
+    );
+  };
+
+  css = () => {
+    return `
+      *, *:before, *:after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      h1, label, input {
+        font-family: ${font.family};
+        font-display: block;
+        color: ${colors.text};
+        line-height: ${font.lineHeight};
+      }
+
+      .app {
+        background: ${colors.section};
+        min-height: 100vh;
+        padding: ${space.xl}  ${space.l};
+
+        display: flex;
+        justify-content: center;
+        align-items: flex-start
+      }
+      
+      .card {
+        background: ${colors.surface};
+        padding: ${space.xxl}  ${space.xl};
+        box-shadow: 0 ${space.xs}  ${space.s} rgb(0 0 0 / 16%);
+        max-width: 35rem;
+        width: 100%;
+        
+        display: grid;
+        grid-template-rows: 1fr;
+        grid-gap: ${space.xxl};
+      }
+
+      h1 {
+        color: ${colors.title};
+        font-size: ${font.size.l};
+        font-weight: bold;
+      }
+
+      .form {
+        display: grid;
+        grid-template: auto 3rem / minmax(10rem, 1fr) minmax(auto, 8rem);
+        grid-template-areas: "label label"
+          "input submit";
+        grid-gap: ${space.l};
+      }
+
+      .add-label {
+        grid-area: label;
+        font-size: ${font.size.s};
+        cursor: pointer;
+      }
+
+      .add-input {
+        grid-area: input;
+        font-size: ${font.size.l};
+        font-weight: bold;
+        padding: ${space.l};
+        background-color: ${colors.lightGray};
+        color: ${colors.input.text.default};
+        border: 0 solid ${colors.input.border.default};
+        border-bottom-width: ${space.s};
+        outline: none;
+        width: 100%;
+        transition: all 0.2s ease-out;
+        transition-property: color, border-color;
+      }
+
+      .add-input:hover,
+      .add-input:focus {
+        color: ${colors.input.text.hover};
+        border-color: ${colors.input.border.hover};
+      }
+
+      .add-button {
+        grid-area: submit;
+        appearance: button;
+        cursor: pointer;
+        font-size: ${font.size.s};
+        color: ${colors.button.text.default};
+        background-color: ${colors.button.background.default};
+        border: ${space.xs} solid ${colors.button.border.default};
+        outline: 0;
+        transition: all 0.2s ease-out;
+        transition-property: background-color, color, border-color;
+      }
+
+      .add-button:hover,
+      .add-button:focus {
+        background-color: ${colors.button.background.hover};
+        color: ${colors.button.text.hover};
+        border-color: ${colors.button.border.hover};
+      }
+      
+      .add-button:active {
+        background-color: ${colors.button.background.active};
+        color: ${colors.button.text.active};
+        border-color: ${colors.button.border.active};
+      }      
+
+      .list {
+        list-style: none;
+        display: grid;
+        grid-template-columns: 1fr;
+      }
+    `;
   };
 }
--- src/todo-item-component.tsx
+++ src/todo-item-component.tsx
@@ -1,38 +1,152 @@
-import { HisafeElement } from "./hisafe-element.js";
-import hisafe from "./hisafe.js";
-import { TodoItem } from "./todo-item.js";
+import { HisafeElement } from './hisafe-element.js';
+import hisafe from './hisafe.js';
+import { TodoItem } from './todo-item.js';
+import { colors, space, font } from './theme.js';
 
 export class TodoItemComponent extends HisafeElement<TodoItem> {
   constructor() {
-    super({ label: "", isDone: false, id: "" });
+    super({ label: '', isDone: false, id: '' });
   }
 
-  css = () => {
-    return `.isDone {text-decoration:line-through}`;
-  };
-
   html(): Node {
+    const svg = `<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+    width="20"
+    height="20"
+    class="delete-icon">
+    <path
+      fill="currentColor"
+      d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"></path>
+  </svg>`;
+
     const myElement = (
-      <li>
-        <input
-          type="checkbox"
-          onChange={this.handleCheckboxChanged}
-          checked={this.state.isDone}
-        />
-        <span class={this.state.isDone ? "isDone" : ""}>
+      <li class={`item ${this.state.isDone ? 'done' : ''}`}>
+        <div className="checkbox">
+          <input
+            type="checkbox"
+            onChange={this.handleCheckboxChanged}
+            checked={this.state.isDone}
+            id={this.state.id}
+          />
+        </div>
+
+        <label for={this.state.id} class="text">
           {this.state.label}
-        </span>
-        <button onClick={this.handleDelete}>X</button>
+        </label>
+
+        <button
+          aria-label={`Delete todo '${this.state.label}'`}
+          onClick={this.handleDelete}
+          class="delete-button"
+          innerHTML={svg}></button>
       </li>
     );
     return myElement;
   }
 
   handleDelete = () => {
-    this.dispatchHisafeEvent("deleteTodoItem", this.state.id);
+    this.dispatchHisafeEvent('deleteTodoItem', this.state.id);
   };
 
   handleCheckboxChanged = () => {
     this.state.isDone = !this.state.isDone;
+  };
+
+  css = () => {
+    return `
+      *, *:before, *:after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+    
+      label, button {
+        font-family: ${font.family};
+        font-display: block;
+        color: ${colors.text};
+        line-height: ${font.lineHeight};
+      }
+
+      .item {
+        padding: ${space.m} 0;
+        width: 100%;
+        display: grid;
+        grid-template-columns: auto 1fr ${space.xl};
+        grid-gap: 1rem;
+        align-items: center;
+        border-bottom: ${space.xxs}  solid ${colors.lightGray};
+      }
+
+      .checkbox {
+        border: ${space.xs} solid ${colors.lightGray};
+        background-color: transparent;
+        width: 1.5rem;
+        aspect-ratio: 1/1;
+        position: relative;
+        transition: all 0.2s ease-out;
+        transition-property: border-color, background-color;
+      }
+
+      .checkbox:before {
+        content: '';
+        display: block;
+        width: 18%;
+        height: 65%;
+        transform: translate(-50%, -50%) rotate(45deg);
+        border-color: transparent;
+        border-style: solid;
+        border-width: 0 ${space.xs} ${space.xs} 0;
+        position: absolute;
+        top: 46%;
+        left: 50%;
+        pointer-events: none;
+        transition: border-color 0.2s ease-out;
+      }
+
+      .done .checkbox {
+        background-color: ${colors.lightGray};
+      }
+
+      .done .checkbox:before {
+        border-color: ${colors.primary};
+      }
+
+      .checkbox > input {
+        appearance: none;
+        display: block;
+        width: 100%;
+        height: 100%;
+        cursor: pointer;
+      }
+
+      .text {
+        font-size: ${font.size.m};
+        cursor: pointer;
+      }
+
+      .done .text {
+        text-decoration: line-through;
+      }
+
+      .delete-button {
+        appearance: none;
+        aspect-ratio: 1/1;
+        font-size: ${font.size.m};
+        cursor: pointer;
+        color: ${colors.gray};
+        background: transparent;
+        border: none;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: color 0.2s ease-out;
+      }
+
+      .delete-button:hover,
+      .delete-button:focus {
+        color: ${colors.button.background.hover};
+      }
+    `;
   };
 }
